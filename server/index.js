6d5f32ff58466cccbef08394564a596cb16264e6
Kiss Lívia <alivedesignstudio00@gmail.com>
2026-03-01 11:49:58 +0100

    add root route to serve index.html

--- server/index.js
+++ server/index.js
@@ -84,6 +84,10 @@ ${data['payment-method']}
   }
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 // ── Start server ──
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
